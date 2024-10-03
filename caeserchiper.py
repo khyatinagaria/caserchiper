13ca5603ff492eb8ef50af409f2af9798cc5a5c3
khyatinagaria <146091358+khyatinagaria@users.noreply.github.com>
2024-10-03 18:55:42 +0530

    Add files via upload

---
+++ caeserchiper.py
@@ -0,0 +1,53 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+logo=('''     __                     
+     (_)     | |              
+  ___ _ _ __ | |__   ___ _ __ 
+ / __| | '_ \| '_ \ / _ \ '__|
+| (__| | |_) | | | |  __/ |   
+ \___|_| .__/|_| |_|\___|_|   
+       | |                    
+       |_|                                  ''')
+
+print(logo)
+# def code(start_text, shift_amount,cipher_direction):
+#     end_text =""
+#     if cipher_direction =="decode":
+#         shift_amount *= -1
+#     for letter in start_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         end_text += alphabet[new_position]
+#     print(f"The {direction} text is {end_text}")
+def code(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+code(start_text=text,shift_amount=shift,cipher_direction=direction)
+
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    code(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
